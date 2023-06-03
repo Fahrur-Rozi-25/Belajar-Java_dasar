d77b4dc93a9ec675aefebc4b2c6278ead53f5030
Fahrur <akungamesaya1234566@gmail.com>
2023-06-03 13:45:37 +0700

    Java

--- src/konversionDataNumber.java
+++ src/konversionDataNumber.java
@@ -3,6 +3,7 @@ public class konversionDataNumber {
     public static void main(String[] args) {
         byte iniByte = 100;
         short iniShort = iniByte;
+        char ini = '1';
         int iniInt = iniShort;
         iniByte = (byte) 200_000_000;
 
