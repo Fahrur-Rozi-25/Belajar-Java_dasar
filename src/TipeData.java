21469ef084e75d85785531c7ebff2c71b5b0bc7e
Fahrur <akungamesaya1234566@gmail.com>
2023-06-03 13:38:28 +0700

    Java

---
+++ src/TipeData.java
@@ -0,0 +1,22 @@
+public class TipeData {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 100000000;
+        long iniLong = 1000000000000000L;
+
+
+        float iniFloat = 100.10f;
+        double iniDouble = 200.1;
+
+
+        int decimal = 230;
+        int hexadecimal = 0xA12B221;
+        int binary = 0b010110010001;
+
+        int Balance = 10_000;
+
+
+
+    }
+}
---
+++ src/konversionDataNumber.java
@@ -0,0 +1,11 @@
+public class konversionDataNumber {
+
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+        iniByte = (byte) 200_000_000;
+
+
+    }
+}
